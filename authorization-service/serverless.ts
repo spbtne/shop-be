270220b2cfbb550f111a10ddc2b263ccaf7211c3
Nikita Tushin <nikita_tushin@epam.com>
2023-11-07 23:49:07 +0300

    task-7

---
+++ authorization-service/serverless.ts
@@ -0,0 +1,41 @@
+import type { AWS } from '@serverless/typescript'
+import { basicAuthorizer } from '@functions'
+
+const serverlessConfiguration: AWS = {
+    service: 'authorization-service',
+    frameworkVersion: '3',
+    plugins: ['serverless-esbuild', 'serverless-dotenv-plugin'],
+    useDotenv: true,
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs18.x',
+        region: 'eu-west-1',
+        profile: 'instructor',
+        stage: 'dev',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+            NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+            spbtne: '${env:spbtne}',
+        },
+    },
+    functions: { basicAuthorizer },
+    package: { individually: true },
+    custom: {
+        esbuild: {
+            bundle: true,
+            minify: true,
+            sourcemap: true,
+            exclude: ['aws-sdk'],
+            target: 'node18',
+            define: { 'require.resolve': undefined },
+            platform: 'node',
+            concurrency: 10,
+        },
+    },
+}
+
+module.exports = serverlessConfiguration
